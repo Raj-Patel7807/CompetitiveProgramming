403d1ed80e0f081a5901e7ce120a63409877aacc
Raj Patel <rajpatel7807@gmail.com>
2025-01-12 16:56:55 +0530

    Update Template

--- CP_Template.cpp
+++ CP_Template.cpp
@@ -23,7 +23,7 @@
 /*
     @Raj_Patel_7807
     Code By : Raj_Patel
-    Date : 26/12/2024    Time -> 20:35:10
+    Date : 12/01/2025    Time -> 16:55:42
     Problem : CPdefault
 */
 
@@ -69,8 +69,12 @@ using usl = unordered_set<ll>;
 #define vin(type, name, n) vector<type> name(n); for(ll i=0; i<(n); ++i) cin >> name[i];
 
 // Outputs
-inline void YES(bool flag = true) { if(flag) {cout << "YES\n";} else {cout << "NO\n";} }
-inline void NO(bool flag = true) { if(flag) {cout << "NO\n";} else {cout << "YES\n";} }
+inline void YES(bool flag = true, bool small = false) {
+    if(flag) { cout << (small ? "Yes\n" : "YES\n"); } else { cout << (small ? "No\n" : "NO\n"); }
+}
+inline void NO(bool flag = true, bool small = false) {
+    if(flag) { cout << (small ? "No\n" : "NO\n"); } else { cout << (small ? "Yes\n" : "YES\n"); }
+}
 #define out(n) cout << (n) << '\n';
 #define outt(n) cout << (n) << ' ';
 #define vout(a) for(auto x : a) { cout << x << ' '; } cout << '\n';
@@ -120,9 +124,8 @@ const ll MOD = 1e9 + 7;
 const ll MOD1 = 998244353;
 
 // Functions
-#define countBit __builtin_popcount
-#define countBitll __builtin_popcountll
-inline ll gcd(ll a, ll b) { return (b == 0) ? a : gcd(b, a % b); }
+#define cntSetBit __builtin_popcountll
+inline ll gcd(ll a, ll b) { while(b) { a %= b; swap(a, b); } return a; }
 inline ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
 inline bool isPrime(ll n) {
     if(n <= 1) return false; if(n <= 3) return true; if(n % 2 == 0 || n % 3 == 0) return false;
@@ -150,6 +153,7 @@ inline bool isPerfectSq(ll n) { if(n < 0) return false; ll sr = static_cast<ll>(
 inline bool compbyss(const pair<ll, ll>& a, const pair<ll, ll>& b) {
     if(a.second == b.second) { return a.first < b.first; } return a.second < b.second;
 }
+inline ll msbPos(ll n) { if(n == 0) { return -1; } return 63 - __builtin_clzll(n); }
 inline ll modsum(ll a, ll b, ll mod = MOD) { return (a % mod + b % mod) % mod; }
 inline ll modsub(ll a, ll b, ll mod = MOD) { return ((a % mod - b % mod) + mod) % mod; }
 inline ll modmult(ll a, ll b, ll mod = MOD) { return (a % mod * b % mod) % mod; }
@@ -161,7 +165,7 @@ inline bool checkbit(ll n, ll pos) { return n & (1LL << pos); }
 
 //=======================^===================================================================^=======================\\
 
-void solve() {
+inline void solve(ll tt) {
     
 }
 
@@ -179,7 +183,7 @@ signed main() {
     // cin >> tt;
 
     while(tt--) {
-        solve();
+        solve(tt);
     }
 
     return 0;
