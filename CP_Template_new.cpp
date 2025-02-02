c74897b73b20bf7e65e1e1b4fc5c2720bb2d701c
Raj-Patel7807 <rajpatel7807@gmail.com>
2025-02-02 22:40:14 +0530

    Update Header and CP_Template

--- CP_Template_new.cpp
+++ CP_Template_new.cpp
@@ -6,64 +6,57 @@
 //===============================================//
 
 #include <bits/stdc++.h>
-// #include <ext/pb_ds/assoc_container.hpp>
-// #include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
-// using namespace __gnu_pbds;
-
-#define Author ios :: sync_with_stdio(false);
-#define is cin.tie(nullptr);
-#define RAJ_PATEL cout.tie(nullptr)
-#define endl '\n'
-#define rr return
-#define precision(x) cout << fixed << setprecision(x)
 
 using ll = long long int;
+using ull = unsigned long long int;
 using db = double;
 using ld = long double;
-template <typename T> using vc = vector<T>;
-template <typename T> using vvc = vector<vector<T>>;
-template <typename T, typename V> using vcp = vector<pair<T, V>>;
+template <typename T> using VC = vector<T>;
+template <typename T> using VVC = vector<vector<T>>;
+template <typename T, typename V> using VCP = vector<pair<T, V>>;
 template <typename T, typename V> using umap = unordered_map<T, V>;
 template <typename T> using uset = unordered_set<T>;
 template <typename T> using stk = stack<T>;
 template <typename T> using qu = queue<T>;
 template <typename T> using pq = priority_queue<T>;
-template <typename T> using pqmin = priority_queue<T, vector<T>, greater<T>>;
-// using idset = tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>;
+template <typename T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+using idset = tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>;
 #define int ll
 
-//========== Loops ==========//
-#define FOR(i, s, e) for(ll i=(s); i<=(e); ++i)
-#define FORA(i, s, e, inc) for(ll i=(s); i<=(e); i+=(inc))
-#define FORR(i, s, e) for(ll i=(s); i>=(e); --i)
+#define FOR1(s) for(ll i=0; i<(s); ++i)
+#define FOR2(i, s) for(ll i=0; i<(s); ++i)
+#define FOR3(i, s, e) for(ll i=(s); i<(e); ++i)
+#define FOR4(i, s, e, inc) for(ll i=(s); i<(e); i+=(inc))
+#define FORR1(s) for(ll i=(s); i>=0; --i)
+#define FORR2(i, s) for(ll i=(s); i>=0; --i)
+#define FORR3(i, s, e) for(ll i=(s); i>=(e); --i)
+#define GET_FOR(a, b, c, d, LOOP, ...) LOOP
+#define GET_FORR(a, b, c, LOOP, ...) LOOP
+#define FOR(...) GET_FOR(__VA_ARGS__, FOR4, FOR3, FOR2, FOR1)(__VA_ARGS__)
+#define FORR(...) GET_FORR(__VA_ARGS__, FORR3, FORR2, FORR1)(__VA_ARGS__)
 #define FORE(x, a) for(auto& x : (a))
 
-//========== Input ==========//
-template <typename... T> inline void INPUT(T&... args) {
-    ((cin >> args), ...);
-}
-#define IN(type, ...) type __VA_ARGS__; INPUT(__VA_ARGS__);
-#define VIN(type, name, n) vector<type> name(n); for(ll i=0; i<(n); ++i) cin >> name[i];
-
-//========== Output ==========//
-inline void YES(bool flag = true, bool small = false) {
-    cout << (flag ? (small ? "Yes\n" : "YES\n") : (small ? "No\n" : "NO\n"));
-}
-inline void NO(bool flag = true, bool small = false) {
-    cout << (flag ? (small ? "No\n" : "NO\n") : (small ? "Yes\n" : "YES\n"));
-}
-template <typename... T> inline void OUTPUT(bool flag, T&... args) {
-    ((cout << args << (flag ? '\n' : ' ')), ...);
-}
+template <typename... T> inline void INPUT(T&... args) { ((cin >> args), ...); }
+template <typename... T> inline void OUTPUT(bool F, T&... args) { ((cout << args << (F ? '\n' : ' ')), ...); }
+#define IN(T, ...) T __VA_ARGS__; INPUT(__VA_ARGS__);
 #define OUT(...) OUTPUT(true, __VA_ARGS__);
 #define OUTT(...) OUTPUT(false, __VA_ARGS__);
+#define VIN(T, var, n) vector<T> name(n); for(ll i=0; i<(n); ++i) cin >> var[i];
+#define VVIN(T, var, n, m) VC<VC<T>> name(n, VC<T>(m)); FOR(i, n) FOR(j, m) cin >> var[i][j];
 #define VOUT(a) for(auto x : a) { cout << x << ' '; } cout << '\n';
+inline void YES(bool F = true, bool S = false) { cout << (F ? (S ? "Yes\n" : "YES\n") : (S ? "No\n" : "NO\n")); }
+inline void NO(bool F = true, bool S = false) { cout << (F ? (S ? "No\n" : "NO\n") : (S ? "Yes\n" : "YES\n")); }
 
-//========== Utils ==========//
+#define endl '\n'
+#define bcn cout << '\n';
+#define OK return
 #define PB push_back
 #define EB emplace_back
-#define ins insert
+#define INS insert
 #define PPB pop_back
 #define MP make_pair
 #define FF first
@@ -73,107 +66,82 @@ template <typename... T> inline void OUTPUT(bool flag, T&... args) {
 #define LEN(x) ll((x).size())
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
-#define ssort(x) sort((x).begin(), (x).end())
+#define sort(x) sort((x).begin(), (x).end())
 #define rsort(x) sort((x).rbegin(), (x).rend())
 #define maxval(v) *max_element((v).begin(), (v).end())
 #define minval(v) *min_element((v).begin(), (v).end())
 #define maxid(v) max_element((v).begin(), (v).end()) - ((v).begin())
 #define minid(v) min_element((v).begin(), (v).end()) - ((v).begin())
-// rotate(v.begin(),v.begin()+v.size()-r,v.end()); // for rotating vector r times right;
-// rotate(v.begin(),v.begin()+r,v.end()); // For rotating vector r times left;
 
-//========== Debugging ==========//
 #ifndef ONLINE_JUDGE
-#include "../Header/debug.hpp"
+#include "Header/debug.hpp"
 #else
 #define debug(x) 7807
 #endif
 
-//========== Constants ==========//
 const ld PI = 3.1415926535897932384626L;
 const ld E = 2.7182818284590452353602L;
 const ll INF = 1e18 + 9;
 const ll MOD = 1e9 + 7;
 const ll MOD1 = 998244353;
 
-//========== Functions ==========//
 #define cntSetBit __builtin_popcountll
 inline ll gcd(ll a, ll b) { while(b) { a %= b; swap(a, b); } return a; }
-inline ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+inline ll lcm(ll a, ll b) { return ((a / gcd(a, b)) * b); }
 inline bool isPrime(ll n) {
-    if(n <= 1) return false; if(n <= 3) return true;
-    if(n % 2 == 0 || n % 3 == 0) return false;
-    for(ll i=5; i*i<=n; i+=6) {
-        if(n % i == 0 || n % (i+2) == 0) return false;
-    }
-    return true;
+    if(n <= 1) return false; if(n <= 3) return true; if(n % 2 == 0 || n % 3 == 0) return false;
+    for(ll i=5; i*i<=n; i+=6) { if(n % i == 0 || n % (i+2) == 0) return false; } return true;
 }
-void prime(ll n, vector<bool>& isprime) {
-    isprime.resize(n+1, true); isprime[0] = isprime[1] = false;
-    for(ll i=2; i*i<=n; i++) {
-        if(isprime[i]) {
-            for(ll j=i*i; j<=n; j+=i) { isprime[j] = false; }
-        }
-    }
+void prime(ll n, vector<bool>& ispr) {
+    ispr.resize(n+1, true); ispr[0] = ispr[1] = false;
+    for(ll i=2; i*i<=n; i++) { if(ispr[i]) { for(ll j=i*i; j<=n; j+=i) { ispr[j] = false; } } }
 }
-void prime(ll n, vector<ll>& primes) {
-    vector<bool> isprime; prime(n, isprime);
-    for(ll i=2; i<=n; i++) {
-        if(isprime[i]) { primes.push_back(i); }
-    }
+void prime(ll n, vector<ll>& pr) {
+    vector<bool> ispr; prime(n, ispr); for(ll i=2; i<=n; i++) { if(ispr[i]) { pr.push_back(i); } }
 }
 inline ll power(ll a, ll b, ll mod = MOD) {
-    ll ans = 1; a %= mod;
-    while(b > 0) {
-        if(b & 1) { ans = (ans * a) % mod; }
-        a = (a * a) % mod;
-        b >>= 1;
-    }
-    return ans;
+    ll ans = 1; a %= mod; while(b > 0) { if(b & 1) { ans = (ans * a) % mod; } a = (a * a) % mod; b >>= 1; } return ans;
 }
-inline void factorial(ll n, vector<ll>& facts) {
-    facts.resize(n+1, 1);
-    for(ll i=1; i<=n; i++) { facts[i] = (facts[i-1] * i) % MOD; }
-}
-inline bool isPowOfTwo(ll n) {
-    return n > 0 && (n & (n-1)) == 0;
-}
-inline bool isPerfectSq(ll n) {
-    if(n < 0) return false; ll sr = static_cast<ll>(sqrt(n));
-    return (sr*sr == n);
-}
-inline bool compbyss(const pair<ll, ll>& a, const pair<ll, ll>& b) {
-    if(a.second == b.second) { return a.first < b.first; }
-    return a.second < b.second;
-}
-inline ll msbPos(ll n) { if(n == 0) { return -1; } return 63 - __builtin_clzll(n); }
-inline ll modadd(ll a, ll b, ll mod = MOD) { return (a % mod + b % mod) % mod; }
-inline ll modmult(ll a, ll b, ll mod = MOD) { return (a % mod * b % mod) % mod; }
+inline void factorial(ll n, vector<ll>& a) { a.resize(n+1, 1); for(ll i=1; i<=n; i++) { a[i] = (a[i-1] * i) % MOD; } }
+inline bool isPowOfTwo(ll n) { return ((n > 0) && !(n & (n-1))); }
+inline bool isPerfectSq(ll n) { if(n < 0) return false; ll sr = static_cast<ll>(sqrt(n)); return (sr*sr == n); }
+inline bool compbyss(const pair<ll, ll>& a, const pair<ll, ll>& b) { return a.second < b.second; }
+inline ll msbPos(ll n) { if(n == 0) { return -1; } return (63 - (__builtin_clzll(n))); }
+inline ll modadd(ll a, ll b, ll mod = MOD) { return ((a % mod + b % mod) % mod); }
+inline ll modmult(ll a, ll b, ll mod = MOD) { return ((a % mod * b % mod) % mod); }
 inline ll modinv(ll a, ll mod = MOD) { return power(a, mod-2, mod); }
 inline ll moddiv(ll a, ll b, ll mod = MOD) { return modmult(a, modinv(b, mod), mod); }
+inline ll TT(bool flag = false) { ll tt = 1; if(flag) { cin >> tt; } return tt; }
+inline void SETUP_IO(bool FILE_IO = false) {
+    ios_base :: sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    if(FILE_IO) {
+        #ifndef ONLINE_JUDGE
+            freopen("Input.txt", "r", stdin);
+            freopen("Output.txt", "w", stdout);
+            freopen("Debug.txt", "w", stderr);
+        #endif
+    }
+}
 
-//==================== Code ====================//
+//==========^==========<<   C O D E   B Y   R A J  P A T E L   >>==========^==========//
+
 inline void solve(ll tt) {
     
 }
 
-//========== main Function ==========//
 signed main() {
+    SETUP_IO();
 
-    Author is RAJ_PATEL;
-
-    #ifndef ONLINE_JUDGE
-        freopen("Input.txt", "r", stdin);
-        freopen("Output.txt", "w", stdout);
-        freopen("Error.txt", "w", stderr);
-    #endif
-
-    ll tt = 1;
-    // cin >> tt;
-
-    for(ll i = 0; i < tt; i++) {
-        solve(i);
-    }
+    ll tt = TT();
+    FOR(tt) solve(i);
 
     return 0;
 }
+
+// NOTE:
+// rotate(v.begin(), v.begin()+v.size()-r, v.end()); - for rotating vector r times right;
+// rotate(v.begin(), v.begin()+r, v.end()); - For rotating vector r times left;
+// For `idset`:
+//     find_by_order(k) - O(log n) - Find the k-th smallest element (0-based index);
+//     order_of(value) - O(log n) - Find the index of a value in the sorted set;
+//     Can find the k-th smallest element, or get the position of a value in sorted order;
---
+++ Header/debug.hpp
@@ -0,0 +1,177 @@
+#ifndef DEBUG_HPP
+#define DEBUG_HPP
+
+#include <bits/stdc++.h>
+
+#define debug1(a) cerr << #a << " = "; debug_print(a); cerr << '\n';
+#define debug2(a, b) cerr << #a << " = "; debug_print(a); cerr << '\n'; \
+                     cerr << #b << " = "; debug_print(b); cerr << '\n';
+#define debug3(a, b, c) cerr << #a << " = "; debug_print(a); cerr << '\n'; \
+                        cerr << #b << " = "; debug_print(b); cerr << '\n'; \
+                        cerr << #c << " = "; debug_print(c); cerr << '\n';
+#define debug4(a, b, c, d) cerr << #a << " = "; debug_print(a); cerr << '\n'; \
+                           cerr << #b << " = "; debug_print(b); cerr << '\n'; \
+                           cerr << #c << " = "; debug_print(c); cerr << '\n'; \
+                           cerr << #d << " = "; debug_print(d); cerr << '\n';
+#define debug5(a, b, c, d, e) cerr << #a << " = "; debug_print(a); cerr << '\n'; \
+                              cerr << #b << " = "; debug_print(b); cerr << '\n'; \
+                              cerr << #c << " = "; debug_print(c); cerr << '\n'; \
+                              cerr << #d << " = "; debug_print(d); cerr << '\n'; \
+                              cerr << #e << " = "; debug_print(e); cerr << '\n';
+
+#define GET_DEBUG(a, b, c, d, e, NAME, ...) NAME
+#define debug(...) GET_DEBUG(__VA_ARGS__, debug5, debug4, debug3, debug2, debug1)(__VA_ARGS__)
+
+
+template <typename T> void debug_print(T t);
+template <typename T, typename V> void debug_print(pair<T, V> p);
+template <typename T> void debug_print(vector<T> v);
+template <typename T> void debug_print(set<T> v);
+template <typename T> void debug_print(unordered_set<T> v);
+template <typename T> void debug_print(multiset<T> v);
+template <typename T, typename V> void debug_print(map<T, V> v);
+template <typename T, typename V> void debug_print(unordered_map<T, V> v);
+template <typename T> void debug_print(deque<T> v);
+template <typename T> void debug_print(stack<T> s);
+template <typename T> void debug_print(queue<T> q);
+template <typename T> void debug_print(priority_queue<T> pq);
+template <typename T, typename C> void debug_print(priority_queue<T, vector<T>, C> pq);
+
+template <typename T>
+void debug_print(T t) {
+    cerr << t;
+}
+
+template <typename T, typename V>
+void debug_print(pair<T, V> p) {
+    cerr << "{";
+    debug_print(p.first);
+    cerr << ", ";
+    debug_print(p.second);
+    cerr << "}";
+}
+
+template <typename T>
+void debug_print(vector<T> v) {
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        debug_print(i);
+        cerr << ' ';
+    }
+    cerr << "]";
+}
+
+template <typename T>
+void debug_print(set<T> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        debug_print(i);
+        cerr << ' ';
+    }
+    cerr << "]";
+}
+
+template <typename T>
+void debug_print(unordered_set<T> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        debug_print(i);
+        cerr << ' ';
+    }
+    cerr << "]";
+}
+
+template <typename T>
+void debug_print(multiset<T> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        debug_print(i);
+        cerr << ' ';
+    }
+    cerr << "]";
+}
+
+template <typename T, typename V>
+void debug_print(map<T, V> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        cerr << "{";
+        debug_print(i.first);
+        cerr << ", ";
+        debug_print(i.second);
+        cerr << "}";
+        cerr << ' ';
+    }
+    cerr << "]";
+}
+
+template <typename T, typename V>
+void debug_print(unordered_map<T, V> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        cerr << "{";
+        debug_print(i.first);
+        cerr << ", ";
+        debug_print(i.second);
+        cerr << "}";
+        cerr << ' ';
+    }
+    cerr << "]";
+}
+
+template <typename T>
+void debug_print(deque<T> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        debug_print(i);
+        cerr << ' ';
+    }
+    cerr << "]";
+}
+
+template <typename T>
+void debug_print(stack<T> s) {
+    cerr << "[ ";
+    while (!s.empty()) {
+        debug_print(s.top());
+        cerr << ' ';
+        s.pop();
+    }
+    cerr << "]";
+}
+
+template <typename T>
+void debug_print(queue<T> q) {
+    cerr << "[ ";
+    while (!q.empty()) {
+        debug_print(q.front());
+        cerr << ' ';
+        q.pop();
+    }
+    cerr << "]";
+}
+
+template <typename T>
+void debug_print(priority_queue<T> pq) {
+    cerr << "[ ";
+    while (!pq.empty()) {
+        debug_print(pq.top());
+        cerr << ' ';
+        pq.pop();
+    }
+    cerr << "]";
+}
+
+template <typename T, typename C>
+void debug_print(priority_queue<T, vector<T>, C> pq) {
+    cerr << "[ ";
+    while (!pq.empty()) {
+        debug_print(pq.top());
+        cerr << ' ';
+        pq.pop();
+    }
+    cerr << "]";
+}
+
+#endif
